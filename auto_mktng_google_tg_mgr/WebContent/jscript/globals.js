a7011d1226f05bd43af2903e4a5a33126e3dadd9
enrico200165 <ocirne00>
2018-05-17 16:00:07 +0100

    code starts working

--- auto_mktng_google_tg_mgr/WebContent/jscript/globals.js
+++ auto_mktng_google_tg_mgr/WebContent/jscript/globals.js
@@ -1,6 +1,7 @@
 var selsMustTrap = [ // "ReadyToGoBar\:https\:\/\/rules\.config\.landrover\.com\/jdxl\/en_gb\/l550"
 	"#button01"
 	,"#div01"
+	,"#div02"
 	,"#span01"
 	,"#ReadyToGoBar:https://rules.config.landrover.com/jdxl/en_gb/l550"
 	];
--- auto_mktng_google_tg_mgr/WebContent/jscript/handlers.js
+++ auto_mktng_google_tg_mgr/WebContent/jscript/handlers.js
@@ -7,8 +7,10 @@ function clickMatches(el, sels, dump = true) {
 	if (!el) {
 		msg("element is null");
 	}
-	
+	msg("\n\n\n---------------------------------------");
+
 	for (i = 0; i < sels.length; i++) {
+				
 		if (sels[i].charAt(0) == '.' || sels[i].charAt(0) == '#') 
 			escSel = sels[i].charAt(0)+jQuery.escapeSelector(sels[i].substring(1));
 		else 
@@ -17,13 +19,13 @@ function clickMatches(el, sels, dump = true) {
 		//descs = jQuery("#div01").find(el);
 		descs = jQuery(escSel).find(el);
 		if (descs.length > 0) {
-				msg("element: " + el+" is descendants of $("+sels[i]+")")
+			msg("element: " + el+" is descendants of $("+sels[i]+")")
 		}
 
 		
 //		console.log(jQuery(escSel)[0]+" - "+el)
 		if (jQuery(escSel).length > 0 && jQuery(escSel)[0] == el ){
-			msg(jQuery(escSel)[0]+" is itself "+ el)				
+			msg(""+jQuery(escSel)[0].tagName +" is itself "+ el+" id: "+ (el.id ? el.id : "no id" ));											
 		}
 		
 /*		
