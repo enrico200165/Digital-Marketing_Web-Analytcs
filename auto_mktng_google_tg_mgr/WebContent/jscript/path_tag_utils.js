b3944cdb92f4d80e7cbc79a71f0fa5f9cc4ea8de
enrico200165 <ocirne00>
2018-05-18 17:26:15 +0100

    completed construction of selector of clicked element testing fixing and refinement necessary, but it starts being there

--- auto_mktng_google_tg_mgr/WebContent/jscript/path_tag_utils.js
+++ auto_mktng_google_tg_mgr/WebContent/jscript/path_tag_utils.js
@@ -10,7 +10,8 @@ function buildPath(e)  {
 	pars = [e];
 	for (i = 0; i < parsTemp.length; i++) { pars.push(parsTemp[i]); }
 	pars.reverse();
-	
+
+	positions = pars.map(function(x) { return $(x).index();});
 	els = pars.map(function(x) { return x.tagName; }); msg("elements: "+els, (new Error()).lineNumber);
 	ids = pars.map(function(x) { return x.id; }); msg("ids: "+ids);
 	classes = pars.map(function(x) { return x.classname; }); msg("classes: "+classes);
@@ -26,21 +27,48 @@ function buildPath(e)  {
 			continue;
 		}
 		
-		dumpMsg = "atributes of ...<"+els[i]+">";
 		// iterate over attributes of ith element
-		ithTagAttribules = {};
+		dumpMsg = "atributes of ...<"+els[i]+">";
+		ithTagAttributes = {};
 		for (attrIdx = 0; attrIdx < elattrs.length; attrIdx++) {
-			name = elattrs[attrIdx].name;
-			value = elattrs[attrIdx].value;
-			ithTagAttributes.push({name: value}); 
-			dumpMsg += " "+name+"= '"+value+"',";
+			name = elattrs[attrIdx].name; value = elattrs[attrIdx].value;
+			ithTagAttributes[name] = value; // dumpMsg += " "+name+"= '"+value+"',";
 		}
-		elAttrs[i] = ithTagAttributes; 
-		msg(dumpMsg);
-	}
+		elAttrs[i] = ithTagAttributes;  // msg(dumpMsg);
 		
+		// build a selector
+		
+	}
 	
-	return true;
+	selector = "";
+	for (i = 0; i < pars.length; i++) {
+		
+		// ID, if present stop, for this ith component
+		selector += (ids[i] ? ("#"+ids[i]) : "");
+		if (ids[i]) {
+			selector += " "; // probably not necessary, just keep for now
+			continue; // TODO restore continue after debugging
+		}
+		
+		selector += els[i].toLowerCase(); // tag
+		
+		noChild = (new Set()).add("html").add("body");
+		if (!noChild.has( els[i].toLowerCase()))  {
+			selector += ":nth-child("+positions[i]+")";
+		}
+		
+		// attributes
+		if ((attrs = elAttrs[i])) {
+			keys = Object.keys(attrs);
+			for (k = 0; k < keys.length; k++) {
+				selector += "["+keys[k]+"="+attrs[keys[k]]+"]";
+			}
+		}
+		
+		if (i <=  pars.length) {selector += " > ";}
+	}
+    msg("selector: "+selector);
+	return selector;
 }
 
 
